6b234599787fc768728ffcf44f5eca139de8a8a2
Wyatt Endres <Wyatt@Wyatts-MacBook-Air.local>
2016-03-19 12:20:42 -0400

    work on socket

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-var express = require('express');
-var app     = express();
-var mongojs = require('mongojs');
-var db = mongojs('copypaste', ['copypaste']);
-var bodyParser = require('body-parser');
-
+var express     = require('express');
+var app         = require('express')();
+var http        = require('http').Server(app);
+var io          = require('socket.io')(http);
+var mongojs     = require('mongojs');
+var db          = mongojs('copypaste', ['copypaste']);
+var bodyParser  = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use(bodyParser.json());
 
 app.get('/', function(req,res){
-    console.log("I received a GET request");
-    });
+    console.log("GET 💰");
+    res.sendFile(__dirname + '/index.html');
+  });
 
-app.listen(3000);
-console.log("Server running on port 3000");
+io.on('connection', function(socket){
+  console.log('a 😀  connected!');
+
+  socket.on('disconnect', function(){
+  console.log('a 😀  disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+  console.log('message: ' + msg);
+  });
+
+});
+
+
+http.listen(3000);
+console.log("Server 👟 👟  on port 3000");
